0583d9076c8daba5a175b6d7ae13eac22a45dd57
Sayed Saminur Rahman <37567927+sayedsamin@users.noreply.github.com>
2025-11-25 14:46:09 -0600

    changed uri for images

--- src/pages/shared/BannerCard.jsx
+++ src/pages/shared/BannerCard.jsx
@@ -1,22 +1,22 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/effect-cards';
+import "swiper/css";
+import "swiper/css/effect-cards";
 
 // banner card styles
-import './bannerCard.css'
+import "./bannerCard.css";
 
 // import required modules
-import { EffectCards } from 'swiper/modules';
+import { EffectCards } from "swiper/modules";
 
 function BannerCard() {
   return (
-    <div className='banner-card'>
+    <div className="banner-card">
       <Swiper
-        effect={'cards'}
+        effect={"cards"}
         grabCursor={true}
         modules={[EffectCards]}
         className="mySwiper"
@@ -28,7 +28,7 @@ function BannerCard() {
         <SwiperSlide></SwiperSlide>
       </Swiper>
     </div>
-  )
+  );
 }
 
-export default BannerCard
+export default BannerCard;
